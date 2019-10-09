3ec069a1758e2b033af188b1f687cb94c43702d4
L1cardo <1014660822@qq.com>
2019-10-09 22:01:37 +0800

    增加摇动手机进行摇骰子的功能

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -18,21 +18,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    // 按键摇骰子
     @IBAction func rollButton(_ sender: Any) {
-        
+        updateDiceImages()
+    }
+    
+    // 摇晃手机
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    // 界面加载
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateDiceImages()
+    }
+
+    // 摇骰子函数
+    func updateDiceImages(){
         index1 = Int.random(in: 0...5)
         index2 = Int.random(in: 0...5)
         
         diceImageView1.image = UIImage(named: diceArray[index1])
         diceImageView2.image = UIImage(named: diceArray[index2])
-        
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
 
 }
 
