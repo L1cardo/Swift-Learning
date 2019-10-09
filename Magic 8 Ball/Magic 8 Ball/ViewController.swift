84dce37ba2fe9e5b25e6289ab77d81022cb749e5
L1cardo <1014660822@qq.com>
2019-10-09 22:32:00 +0800

    添加注释

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let ballImage = ["ball1", "ball2", "ball3", "ball4", "ball5"]
     var index = 0
     
+    // 换8号球函数
     func updateBallImage(){
         index = Int.random(in: 0...4)
         ball.image = UIImage(named: ballImage[index])
@@ -20,15 +21,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ball: UIImageView!
 
+    // 按键
     @IBAction func askButton(_ sender: Any) {
         updateBallImage()
     }
     
+    // 摇手机
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         updateBallImage()
     }
     
-
+    // 界面加载
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
